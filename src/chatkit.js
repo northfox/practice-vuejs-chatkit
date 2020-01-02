dd5fed06d3fd499ab51da06f7562f9e60a9ce1b0
northfox <vulpes.engineer@gmail.com>
2020-01-02 12:25:21 +0900

    Update for subscribe chat room

--- src/chatkit.js
+++ src/chatkit.js
@@ -1,11 +1,13 @@
 import { ChatManager, TokenProvider } from '@pusher/chatkit-client'
+import moment from 'moment'
+import store from './store/index'
 
 const VUE_APP_INSTANCE_LOCATOR = process.env.VUE_APP_INSTANCE_LOCATOR
 const VUE_APP_TOKEN_URL = process.env.VUE_APP_TOKEN_URL
-// const VUE_APP_MESSAGE_LIMIT = Number(process.env.VUE_APP_MESSAGE_LIMIT) || 10
+const VUE_APP_MESSAGE_LIMIT = Number(process.env.VUE_APP_MESSAGE_LIMIT) || 10
 
 let currentUser = null
-// let activeRoom = null
+let activeRoom = null
 
 async function connectUser(userId) {
   const chatManager = new ChatManager({
@@ -17,6 +19,45 @@ async function connectUser(userId) {
   return currentUser
 }
 
+function setMembers() {
+  const members = activeRoom.users.map((user) => ({
+    username: user.id,
+    name: user.name,
+    presence: user.presence.state
+  }))
+  store.commit('setUsers', members)
+}
+
+async function subscribeToRoom(roomId) {
+  store.commit('clearChatRoom')
+  activeRoom = await currentUser.subscribeToRoom({
+    roomId,
+    messageLimit: VUE_APP_MESSAGE_LIMIT,
+    hooks: {
+      onMessage: (message) => {
+        store.commit('addMessage', {
+          name: message.sender.name,
+          username: message.senderId,
+          text: message.text,
+          date: moment(message.createdAt).format('YYYY-MM-D h:mm:ss')
+        })
+      },
+      onPresenceChanged: () => {
+        setMembers()
+      },
+      onUserStartedTyping: (user) => {
+        store.commit('setUserTyping', user.id)
+      },
+      onUserStoppedTyping: () => {
+        store.commit(('setUserTyping', null))
+      }
+    }
+  })
+  setMembers()
+  return activeRoom
+}
+
 export default {
-  connectUser
+  connectUser,
+  subscribeToRoom
 }
--- src/store/actions.js
+++ src/store/actions.js
@@ -10,12 +10,26 @@ export default {
     try {
       commit('setError', '')
       commit('setLoading', true)
+      // user
       const currentUser = await chatkit.connectUser(userId)
       commit('setUser', {
         username: currentUser.id,
         name: currentUser.name
       })
-      console.log(state.user)
+      // rooms
+      const rooms = currentUser.rooms.map((room) => ({
+        id: room.id,
+        name: room.name
+      }))
+      commit('setRooms', rooms)
+      // subscribe
+      const activeRoom = state.activeRoom || rooms[0]
+      commit('setActiveRoom', {
+        id: activeRoom.id,
+        name: activeRoom.name
+      })
+      await chatkit.subscribeToRoom(activeRoom.id)
+      return true
     } catch (error) {
       handleError(commit, error)
     } finally {
