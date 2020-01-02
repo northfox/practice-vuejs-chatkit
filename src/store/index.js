fe3aba2145db775f52beb974f57fb3ce439f6f7c
northfox <vulpes.engineer@gmail.com>
2020-01-02 10:44:02 +0900

    Setting store

--- src/store/index.js
+++ src/store/index.js
@@ -13,10 +13,23 @@ const vuexLocal = new VuexPersistence({
 })
 
 export default new Vuex.Store({
-  state: {},
+  state: {
+    loading: false,
+    sending: false,
+    error: null,
+    user: [],
+    reconnect: false,
+    activeRoom: null,
+    rooms: [],
+    users: [],
+    messages: [],
+    userTyping: null
+  },
   mutations,
   actions,
-  getters: {},
+  getters: {
+    hasError: state => !!state.error
+  },
   plugins: [vuexLocal.plugin],
   strict: debug
 })
